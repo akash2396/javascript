baa0b19dfdc0740cbc29474b16f7891e0a2a7638
akash <akashshingavi@gmail.com>
2023-08-19 11:45:06 +0530

    Declaration of functions

---
+++ Basics5.js
@@ -0,0 +1,31 @@
+// Functions = block of code can be executed by wrapping them in a function
+
+function addNumbers(a,b)
+{
+    return a+b;
+}
+
+sum = addNumbers(2,3)
+
+console.log("sum of two numbers is: "+sum);
+
+//Anyonymus function  --> a function who do not have  name
+
+let sumOfIntegers = (c,d) => c+d
+
+
+console.log("sum of two numbers is: "+sumOfIntegers(20,20));
+
+
+let myName = name => console.log(name);
+
+myName("akash")
+
+
+
+
+
+
+
+
+
